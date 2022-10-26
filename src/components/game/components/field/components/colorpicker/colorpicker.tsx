882b09f60eeb62126be4cb8475e1dcb7af2389b9
sdrahichynski <sdrahichynski@aristeksystems.com>
2022-10-26 13:22:09 +0300

    feat: add color picker

---
+++ src/components/game/components/field/components/colorpicker/colorpicker.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import cx from "classnames";
+import { consts } from "./duck";
+import styles from "./colorpicker.module.scss";
+
+interface ColorpickerProps {
+  value: string;
+
+  onChange(color: string): void;
+}
+
+const Colorpicker: React.FC<ColorpickerProps> = ({ value, onChange }) => {
+  return (
+    <div className={styles.wrapper}>
+      {consts.COLORS.map((color) => (
+        <div
+          className={cx(styles.colorItem, { [styles.active]: color === value })}
+          style={{ backgroundColor: color }}
+          onClick={() => onChange(color)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Colorpicker;
--- src/components/game/components/field/field.tsx
+++ src/components/game/components/field/field.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import styles from "./field.module.scss";
+import {Colorpicker} from "./components";
 
 interface FieldProps {
   value: string[];
@@ -8,6 +9,7 @@ interface FieldProps {
 }
 
 const Field: React.FC<FieldProps> = ({ value, onChange }) => {
+  const [color, setColor] = React.useState("#ea1e23");
   const size = Math.floor(Math.sqrt(value.length));
   const fieldValue = new Array(size)
     .fill(null)
@@ -17,23 +19,27 @@ const Field: React.FC<FieldProps> = ({ value, onChange }) => {
     (index: number): React.MouseEventHandler =>
     () => {
       const newValue = [...value];
-      newValue[index] = "red";
+      newValue[index] = color;
       onChange(newValue);
     };
 
   return (
     <div className={styles.wrapper}>
-      {fieldValue.map((row, rowIndex) => (
-        <div className={styles.row} key={rowIndex}>
-          {row.map((cell, cellIndex) => (
-            <div
-              className={styles.cell}
-              onMouseDown={handleClick(rowIndex * size + cellIndex)}
-              style={{ backgroundColor: cell }}
-            />
-          ))}
-        </div>
-      ))}
+      <Colorpicker value={color} onChange={setColor} />
+
+      <div className={styles.field}>
+        {fieldValue.map((row, rowIndex) => (
+          <div className={styles.row} key={rowIndex}>
+            {row.map((cell, cellIndex) => (
+              <div
+                className={styles.cell}
+                onMouseDown={handleClick(rowIndex * size + cellIndex)}
+                style={{ backgroundColor: cell }}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
